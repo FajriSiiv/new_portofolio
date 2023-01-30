be9203dd6aaebc66511d481ce404b75d8544cbcc
Fajri Siiv <fajrijijin@gmail.com>
2023-01-30 08:22:13 +0700

    analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
-import Theme from '../styles/theme';
+import Theme from "../styles/theme";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
         <Component {...pageProps} />
+        <Analytics />
       </Theme>
     </>
   );
 }
- 
